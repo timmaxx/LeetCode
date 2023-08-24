6005fb675832ebfb7e2dde63af6e75dbc6f018b1
“Maksim <m.n.timchenko@gmail.com>
2023-08-24 16:10:30 +0300

    solve 14. Longest Common Prefix

--- src/main/java/com/leetcode/t0004_LongestCommonPrefix/Solution.java
+++ src/main/java/com/leetcode/t0004_LongestCommonPrefix/Solution.java
@@ -28,6 +28,20 @@ strs[i] consists of only lowercase English letters.
 
 public class Solution {
     public static String longestCommonPrefix(String[] strs) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        br:
+        for (int i = 0; i < strs[0].length(); i++) {
+            char currentChar = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() == i) {
+                    break br;
+                }
+                if (currentChar != strs[j].charAt(i)) {
+                    break br;
+                }
+            }
+            result.append(currentChar);
+        }
+        return result.toString();
     }
 }
