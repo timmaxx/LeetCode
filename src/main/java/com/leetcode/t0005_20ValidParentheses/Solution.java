7ae6c00447b70925eb47b961ec00f0a94f3e8bcd
“Maksim <m.n.timchenko@gmail.com>
2023-08-25 10:06:32 +0300

    solve 20. Valid Parentheses

--- src/main/java/com/leetcode/t0005_20ValidParentheses/Solution.java
+++ src/main/java/com/leetcode/t0005_20ValidParentheses/Solution.java
@@ -1,7 +1,6 @@
 package com.leetcode.t0005_20ValidParentheses;
 
-/*
-* 20. Valid Parentheses
+/* 20. Valid Parentheses
 Easy
 21.6K
 1.4K
@@ -14,29 +13,53 @@ Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 
-
 Example 1:
-
 Input: s = "()"
 Output: true
-Example 2:
 
+Example 2:
 Input: s = "()[]{}"
 Output: true
-Example 3:
 
+Example 3:
 Input: s = "(]"
 Output: false
 
 
 Constraints:
-
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
-* */
+*/
+
+import java.util.Stack;
 
 class Solution {
     public static boolean isValid(String s) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char chFromStr = s.charAt(i);
+            if (chFromStr == '(' || chFromStr == '[' || chFromStr == '{') {
+                stack.push(chFromStr);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                char chFromStackAnti;
+                char chFromStack = stack.pop();
+                if (chFromStack == '(' ) {
+                    chFromStackAnti = ')';
+                } else if ( chFromStack == '[' ) {
+                    chFromStackAnti = ']';
+                } else {
+                    chFromStackAnti = '}';
+                }
+
+                if (chFromStackAnti != chFromStr) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
     }
 }
--- src/test/java/com/leetcode/t0005_20ValidParentheses/SolutionTest.java
+++ src/test/java/com/leetcode/t0005_20ValidParentheses/SolutionTest.java
@@ -5,18 +5,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.Assert.assertEquals;
-
 public class SolutionTest {
     @ParameterizedTest(name = "s = {1}")
     @CsvSource(textBlock = """
-                 true,    "()"
-                 true,    "()[]{}"
-                false,    "(]"
+                 true,    ()
+                 true,    ()[]{}
+                false,    (]
+                false,    ()]
+                false,    (((
+                false,    ({[}])
+                 true,    ({[]})
         """)
     @DisplayName("Sample tests")
     void sampleTests(Boolean expected, String s) {
-        // assertEquals(expected, Solution.isValid(s));
         Assertions.assertEquals(expected, Solution.isValid(s));
     }
 }
