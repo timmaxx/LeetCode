e4e967e60df9e3ec823b3e3b3e2bdde6ffdb8054
“Maksim <m.n.timchenko@gmail.com>
2023-08-29 10:26:47 +0300

    solve 239. Sliding Window Maximum
    But for k = 50000 "Time Limit Exceeded".

--- src/main/java/com/leetcode/t0006_SlidingWindowMaximum/Solution.java
+++ src/main/java/com/leetcode/t0006_SlidingWindowMaximum/Solution.java
@@ -1,11 +1,32 @@
 package com.leetcode.t0006_SlidingWindowMaximum;
 
 public class Solution {
-
-
     public static int[] maxSlidingWindow(int[] nums, int k) {
         int lengthOfWindow = nums.length - k + 1;
         int[] arrOfMax = new int[lengthOfWindow];
+
+        for (int i = 0; i < lengthOfWindow; i++) {
+            int max = nums[i];
+
+            if (        ( i > 0 ) // Для не нулевого окна
+                    &&  ( nums[i - 1] < arrOfMax[i - 1])  // Если выбывший (т.е. слева) меньше максимального в предыдущем окне
+                    &&  ( nums[i + k - 1] < arrOfMax[i - 1])) // Если прибывший (т.е. справа) меньше максимального в предыдущем окне
+            {
+                // Тогда максимум текущего окна совпадает с максимумом предыдущего окна
+                arrOfMax[i] = arrOfMax[i - 1];
+                continue;
+            }
+
+            // Иначе ищем максимум полным перебором
+            for (int j = 1; j < k; j++) {
+                if (nums[i + j] > max) {
+                    max = nums[i + j];
+                }
+            }
+
+            arrOfMax[i] = max;
+        }
+
         return arrOfMax;
     }
 
@@ -23,6 +44,7 @@ public class Solution {
             }
             arrOfMax[i] = max;
         }
+
         return arrOfMax;
     }
 */
