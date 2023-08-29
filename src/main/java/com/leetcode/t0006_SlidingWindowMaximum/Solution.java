e127f29408ad65854cfd16fd25034322ac70d8d4
“Maksim <m.n.timchenko@gmail.com>
2023-08-29 12:23:06 +0300

    solve 239. Sliding Window Maximum

--- src/main/java/com/leetcode/t0006_SlidingWindowMaximum/Solution.java
+++ src/main/java/com/leetcode/t0006_SlidingWindowMaximum/Solution.java
@@ -4,27 +4,51 @@ public class Solution {
     public static int[] maxSlidingWindow(int[] nums, int k) {
         int lengthOfWindow = nums.length - k + 1;
         int[] arrOfMax = new int[lengthOfWindow];
+        int countOfMax = 1;
 
         for (int i = 0; i < lengthOfWindow; i++) {
             int max = nums[i];
+            boolean bruteForce = i == 0; // Для нулевого окна
 
-            if (        ( i > 0 ) // Для не нулевого окна
-                    &&  ( nums[i - 1] < arrOfMax[i - 1])  // Если выбывший (т.е. слева) меньше максимального в предыдущем окне
-                    &&  ( nums[i + k - 1] < arrOfMax[i - 1])) // Если прибывший (т.е. справа) меньше максимального в предыдущем окне
-            {
-                // Тогда максимум текущего окна совпадает с максимумом предыдущего окна
-                arrOfMax[i] = arrOfMax[i - 1];
-                continue;
-            }
-
-            // Иначе ищем максимум полным перебором
-            for (int j = 1; j < k; j++) {
-                if (nums[i + j] > max) {
-                    max = nums[i + j];
+            if (!bruteForce) {
+                if (nums[i + k - 1] > arrOfMax[i - 1]) {// Если прибывший (т.е. справа) больше максимального в предыдущем окне
+                    countOfMax = 1;
+                    arrOfMax[i] = nums[i + k - 1];
+                    continue;
+                } else if (nums[i + k - 1] == arrOfMax[i - 1]) {
+                    countOfMax++;
+                    arrOfMax[i] = nums[i + k - 1];
+                    continue; // bruteForce = false;
                 }
             }
 
-            arrOfMax[i] = max;
+            if (!bruteForce) {
+                if (nums[i - 1] == arrOfMax[i - 1]) { // Если выбывший (т.е. слева) один из максимального в предыдущем окне
+                    // Уменьшаем счетчик максимальных
+                    countOfMax--;
+                }
+            }
+
+            if (!bruteForce) {
+                bruteForce = countOfMax <= 0;
+            }
+
+            if ( !bruteForce) {
+                // Тогда максимум текущего окна совпадает с максимумом предыдущего окна
+                arrOfMax[i] = arrOfMax[i - 1];
+            } else {
+                // Иначе ищем максимум полным перебором
+                for (int j = 1; j < k; j++) {
+                    if (nums[i + j] == max) {
+                        countOfMax++;
+                    } else if (nums[i + j] > max) {
+                        max = nums[i + j];
+                        countOfMax = 1;
+                    }
+                }
+
+                arrOfMax[i] = max;
+            }
         }
 
         return arrOfMax;
--- src/test/java/com/leetcode/t0006_SlidingWindowMaximum/SolutionTest.java
+++ src/test/java/com/leetcode/t0006_SlidingWindowMaximum/SolutionTest.java
@@ -15,5 +15,10 @@ public class SolutionTest {
         int[] arrOfInt2In = { 1, 3, -1, -3, 5, 3, 6, 7};
         int[] arrOfInt2Out = { 3, 3, 5, 5, 6, 7};
         assertArrayEquals( arrOfInt2Out,  Solution.maxSlidingWindow( arrOfInt2In, 3));
+
+
+        int[] arrOfInt3In = { 9, 10, 9, -7, -4, -8, 2, -6};
+        int[] arrOfInt3Out = { 10, 10, 9, 2};
+        assertArrayEquals( arrOfInt3Out,  Solution.maxSlidingWindow( arrOfInt3In, 5));
     }
 }
