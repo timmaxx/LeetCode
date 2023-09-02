f459b8c7de5b3f03c843e052022f332035a5b95d
“Maksim <m.n.timchenko@gmail.com>
2023-09-02 23:08:26 +0300

    solve brut force (Time Limit Exceeded) 121. Best Time to Buy and Sell Stock

--- src/main/java/com/leetcode/t0008_BestTimeToBuyAndSellStock/Solution.java
+++ src/main/java/com/leetcode/t0008_BestTimeToBuyAndSellStock/Solution.java
@@ -2,6 +2,16 @@ package com.leetcode.t0008_BestTimeToBuyAndSellStock;
 
 public class Solution {
     public static int maxProfit(int[] prices) {
-        return 0;
+        int max = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if ( prices[j] - prices[i] > max) {
+                    max = prices[j] - prices[i];
+                }
+            }
+        }
+
+        return max;
     }
 }
